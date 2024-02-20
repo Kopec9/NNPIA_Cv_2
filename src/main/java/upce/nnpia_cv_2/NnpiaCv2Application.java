f77a3b5ce3ba524d997b6bf282c8aed80c9c3f02
Kopec9 <kopecmar69@gmail.com>
2024-02-20 14:16:16 +0100

    Kostra

---
+++ src/main/java/upce/nnpia_cv_2/NnpiaCv2Application.java
@@ -0,0 +1,13 @@
+package upce.nnpia_cv_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaCv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NnpiaCv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/upce/nnpia_cv_2/NnpiaCv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package upce.nnpia_cv_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaCv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
